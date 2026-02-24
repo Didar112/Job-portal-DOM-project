73b52c5b6b02dcc88c0b6872765ed0e360a67c6e
Didar112 <didarulislam4546@gmail.com>
2026-02-24 18:56:03 +0600

    everything except delete

--- scripts/functions.js
+++ scripts/functions.js
@@ -4,7 +4,7 @@ let appliedList =[]
 let interviewList =[]
 let rejectedList=[]
 
-
+let currentStatus='all-btn'
 
 
 const total = document.getElementById('totalCount');
@@ -28,7 +28,7 @@ const interBtn=document.getElementById("inter-btn")
 const rejectBtn=document.getElementById("reject-btn")
 
 const Jobs=document.getElementById('all-job-container')
-
+const jobWrapper = document.getElementById("job-wrapper");
 // Update total counts in every chart
 function updateTotal(){
     total.innerText=Jobs.children.length
@@ -46,38 +46,64 @@ updateTotal()
 // toggle mechanism
 
 function toggleEngine(id){
+
+    currentStatus=id
+
     allbtn.classList.remove("bg-blue-400", "text-white")
     appliedBtn.classList.remove("bg-blue-400", "text-white")
     interBtn.classList.remove("bg-blue-400", "text-white")
     rejectBtn.classList.remove("bg-blue-400", "text-white")
 
     document.getElementById(id).classList.add("bg-blue-400", "text-white")
-
-    if(id==='all-btn'){
+    // if(id==appliedBtn){
+    //     document.querySelector('.no-job-banner').classList.remove("hidden")
+    //     filsec.classList.add("hidden")
+        
+    // }
+     if(id==='all-btn'){
         filsec.classList.add('hidden')
         Jobs.classList.remove('hidden')
+        
+            document.querySelector('.no-job-banner').classList.add("hidden")
+        
     }
 
-    if(id==='inter-btn'){
+    else if(id==='inter-btn'){
         filsec.classList.remove('hidden')
         Jobs.classList.add('hidden')
+        renderInterview()
+        // if(interviewList.length==0){
+        //     document.querySelector('.no-job-banner').classList.remove("hidden")
+        // } else {
+        //      document.querySelector('.no-job-banner').classList.add("hidden")
+        // }
     }
 
 
-    if(id==='reject-btn')
+    else if(id==='reject-btn')
     {
-
+        Jobs.classList.add('hidden')
+        filsec.classList.remove('hidden')
+        renderReject()
+        // if(rejectedList.length==0){
+        //     document.querySelector('.no-job-banner').classList.remove("hidden")
+        // } else {
+        //     document.querySelector('.no-job-banner').classList.add("hidden")
+        // }
     }
+    
 }
 
 
 
 
 // event delegation
-jobContainer.addEventListener("click", function(event){
+jobWrapper.addEventListener("click", function(event){
       if(event.target.classList.contains('int-btn'))
       {
+        
         const currentJob=event.target.parentNode
+        currentJob.querySelector('.appl-btn').innerText="INTERVIEWED" 
       const comName = currentJob.querySelector('.comp-name').innerText
       const post=currentJob.querySelector('.post').innerText
       const salary=currentJob.querySelector('.salary').innerText
@@ -103,23 +129,87 @@ jobContainer.addEventListener("click", function(event){
         interviewList.push(jobCardObj)
        }
 
+
+       rejectedList = rejectedList.filter(item=>item.comName!=jobCardObj.comName)
+
+
        inter.innerText=interviewList.length
-       currentJob.querySelector('.appl-btn').innerText="INTERVIEWED" 
-       
-       console.log(interviewList);
+       reject.innerText=rejectedList.length
+
+        
 
 
+        if(currentStatus==='reject-btn'){
+            renderReject()
+        }
 
-       renderInterview()
       }
 
+
+      else if(event.target.classList.contains('rej-btn'))
+      {
+        const currentJob=event.target.parentNode
+        currentJob.querySelector('.appl-btn').innerText="REJECTED" 
+      const comName = currentJob.querySelector('.comp-name').innerText
+      const post=currentJob.querySelector('.post').innerText
+      const salary=currentJob.querySelector('.salary').innerText
+      const aplBtn=currentJob.querySelector('.appl-btn').innerText
+      const jobDes=currentJob.querySelector('.job-des').innerText
+      const intBtn=currentJob.querySelector('.int-btn').innerText
+      const rejBtn=currentJob.querySelector('.rej-btn').innerText
+
+      const jobCardObj = {
+        comName,
+        post,
+        salary,
+        aplBtn,
+        jobDes,
+        intBtn,
+        rejBtn
+      }
+
+
+       const filter=rejectedList.find(item=> item.comName==jobCardObj.comName)
+
+       if(!filter){
+        rejectedList.push(jobCardObj)
+       }
+
+       interviewList = interviewList.filter(item=>item.comName!=jobCardObj.comName)
+
+       reject.innerText=rejectedList.length
+       console.log(rejectedList);
+       
+       inter.innerText=interviewList.length
+
+    //    renderReject()
+
+       if(currentStatus==='inter-btn')
+        renderInterview()
+      }
 
 })
 
 
-function renderInterview ()
+// Render interview part function
+
+function renderInterview()
 {
     filsec.innerHTML=''
+
+
+    const banner = document.querySelector('.no-job-banner')
+
+    if (interviewList.length === 0) {
+        banner.classList.remove('hidden')
+        return
+    } else {
+        banner.classList.add('hidden')
+    }
+
+    
+
+
 
     for(let item of interviewList){
         console.log(item);
@@ -131,8 +221,8 @@ function renderInterview ()
                 <div class="card-header flex align-middle justify-between">
                     <div>
 
-                        <p class="comp-name text-[#002C5C] text-[17px] font-medium">Mobile First Corp</p>
-                        <p class=" post text-[#64748B]  text-[17px] font-light">React Native Developer</p>
+                        <p class="comp-name text-[#002C5C] text-[17px] font-medium">${item.comName}</p>
+                        <p class=" post text-[#64748B]  text-[17px] font-light">${item.post}</p>
                     </div>
                     <div id="delete-1" class="delete">
 
@@ -140,16 +230,71 @@ function renderInterview ()
                     </div>
 
                 </div>
-                <p class="salary text-[#64748B] pt-5 pb-4">Remote • Full-time • $130,000 - $175,000</p>
+                <p class="salary text-[#64748B] pt-5 pb-4">${item.salary}</p>
 
-                <button class="appl-btn btn font-medium text-[#002C5C] bg-[#EEF4FF] hover:bg-blue-400 w-[150px] border-0">INTERVIEWED</button>
-                <p class="job-des text-[#002C5C]  text-[15px] font-light mt-2 mb-4">Build cross-platform mobile applications
-                    using React Native. Work on products used by millions of users worldwide.</p>
+                <button class="appl-btn btn font-medium text-[#002C5C] bg-[#EEF4FF] hover:bg-blue-400 w-[150px] border-0">${item.aplBtn}</button>
+                <p class="job-des text-[#002C5C]  text-[15px] font-light mt-2 mb-4">${item.jobDes}</p>
 
                 <button
-                    class="int-btn btn font-medium text-green-400 bg-[#EEF4FF] hover:bg-green-400 hover:text-white w-[120px] border-emerald-400 bg-transparent">INTERVIEW</button>
+                    class="int-btn btn font-medium text-green-400 bg-[#EEF4FF] hover:bg-green-400 hover:text-white w-[120px] border-emerald-400 bg-transparent">${item.intBtn}</button>
                 <button
-                    class=" rej-btn btn font-medium text-red-500 bg-[#EEF4FF] hover:bg-red-400 hover:text-white w-[120px] border-red-400 bg-transparent">REJECTED</button>
+                    class="rej-btn btn font-medium text-red-500 bg-[#EEF4FF] hover:bg-red-400 hover:text-white w-[120px] border-red-400 bg-transparent">${item.rejBtn}</button>
+
+
+            </div>
+        </div>
+        
+        
+        `
+        filsec.appendChild(div)
+    }
+
+    
+}
+
+
+
+
+// reject render part
+
+function renderReject()
+{
+    filsec.innerHTML=''
+
+     if(rejectedList.length==0){
+            document.querySelector('.no-job-banner').classList.remove("hidden")
+        } else {
+            document.querySelector('.no-job-banner').classList.add("hidden")
+        }
+
+    for(let item of rejectedList){
+        console.log(item);
+        let div=document.createElement('div')
+        div.innerHTML= `
+
+             <div class="job-holder mt-3">
+            <div class="job-card p-6 bg-base-100 rounded-md">
+                <div class="card-header flex align-middle justify-between">
+                    <div>
+
+                        <p class="comp-name text-[#002C5C] text-[17px] font-medium">${item.comName}</p>
+                        <p class=" post text-[#64748B]  text-[17px] font-light">${item.post}</p>
+                    </div>
+                    <div id="delete-1" class="delete">
+
+                        <i class="fa-regular fa-trash-can"></i>
+                    </div>
+
+                </div>
+                <p class="salary text-[#64748B] pt-5 pb-4">${item.salary}</p>
+
+                <button class="appl-btn btn font-medium text-[#002C5C] bg-[#EEF4FF] hover:bg-blue-400 w-[150px] border-0">${item.aplBtn}</button>
+                <p class="job-des text-[#002C5C]  text-[15px] font-light mt-2 mb-4">${item.jobDes}</p>
+
+                <button
+                    class="int-btn btn font-medium text-green-400 bg-[#EEF4FF] hover:bg-green-400 hover:text-white w-[120px] border-emerald-400 bg-transparent">${item.intBtn}</button>
+                <button
+                    class="rej-btn btn font-medium text-red-500 bg-[#EEF4FF] hover:bg-red-400 hover:text-white w-[120px] border-red-400 bg-transparent">${item.rejBtn}</button>
 
 
             </div>
@@ -166,3 +311,4 @@ function renderInterview ()
 
 
  
+
